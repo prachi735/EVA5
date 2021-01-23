f295281e73804513de498f5d48b1f27017059eaa
Prachi Singh <prachi735@users.noreply.github.com>
2021-01-23 16:47:16 +0530

    Update utils.py

--- Session7/utils.py
+++ Session7/utils.py
@@ -19,8 +19,7 @@ def get_device():
 
 
 def get_misclassified_images(gbn_model,test_loader):
-    
-    
+        
     test_images = []
     target_labels = []
     target_predictions = []
@@ -52,3 +51,13 @@ def plot_results(train_losses,train_acc,test_losses,test_acc):
         ax = axs[axs_pos[i]]
         ax.plot(data[i])
         ax.set_title(i)
+
+def show_misclassified_images(test_images,target_labels,target_predictions,nrow=5, ncol=5):
+  fig, axes = plt.subplots(nrows=nrow, ncols=ncol, figsize=(15,15))
+  fig.subplots_adjust(hspace=0.5)
+  fig.suptitle('Misclassified Images in GBN Model')
+
+  for ax, image, target, prediction in zip(axes.flatten(), test_images,target_labels,target_predictions):
+      ax.imshow(image[0])
+      ax.set(title='target:{t} prediction:{p}'.format(t=target.item(),p=prediction.item()))
+      ax.axis('off')
