c8075d7d951578783537f7998fc4b9cf089f829f
Prachi Singh <prachi735@users.noreply.github.com>
2021-01-23 16:24:56 +0530

    Update utils.py

--- Session7/utils.py
+++ Session7/utils.py
@@ -15,3 +15,25 @@ def get_device():
   use_cuda = torch.cuda.is_available()
   device = torch.device("cuda" if use_cuda else "cpu")
   return device
+
+
+def get_misclassified_images(gbn_model,test_loader,is_gbn,gbn_splits):
+    
+    test_images = []
+    target_labels = []
+    target_predictions = []
+    for img, target in test_loader:
+      prediction = torch.argmax(gbn_model(img), dim=1)
+      test_images.append( img )
+      target_labels.append( target )
+      target_predictions.append( prediction )
+
+    test_images = torch.cat(test_images)
+    target_labels = torch.cat(target_labels)
+    target_predictions = torch.cat(target_predictions)
+    misclassified_index = target_labels.ne(target_predictions).numpy()
+    test_images = test_images[misclassified_index]
+    target_labels = target_labels[misclassified_index]
+    target_predictions = target_predictions[misclassified_index]
+
+    return test_images,target_labels,target_predictions
