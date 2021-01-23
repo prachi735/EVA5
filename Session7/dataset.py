9de850bca07d95217f7d28b71a1f5132b7e0d32d
Prachi Singh <prachi735@users.noreply.github.com>
2021-01-23 14:06:51 +0530

    Update dataset.py

--- Session7/dataset.py
+++ Session7/dataset.py
@@ -1,13 +1,13 @@
 import torch
-import torchvision
+from torchvision import datasets, transforms, utils
 
 def get_data(name):
     if name == 'MNIST':
-        train = datasets.MNIST('./data', train=True, download=True,transform=train_transforms)
-        test = datasets.MNIST('./data', train=True, download=True,transform=test_transforms)
+        train = datasets.MNIST('./data', train=True, download=True)
+        test = datasets.MNIST('./data', train=True, download=True)
     elif name == 'CIFAR10':
-        train_set = datasets.CIFAR10(root='./data', train=True,download=True,transform=train_transforms)
-        test_set  = datasets.CIFAR10(root='./data', train=False,download=True,transform=test_transforms)
+        train_set = datasets.CIFAR10(root='./data', train=True,download=True)
+        test_set  = datasets.CIFAR10(root='./data', train=False,download=True)
     
     return(train_set,test_set)
     
