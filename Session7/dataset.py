96f7f06179e5e7085e83ee8e3127aaf62e3f3ace
Prachi735 <Prachi735@gmail.com>
2021-01-23 12:48:36 +0530

    updating session 7 assignment files

--- Session7/dataset.py
+++ Session7/dataset.py
@@ -1,71 +1,48 @@
-train_transforms = transforms.Compose([
-                                      #  transforms.Resize((28, 28)),
-                                      #  transforms.ColorJitter(brightness=0.10, contrast=0.1, saturation=0.10, hue=0.1),
-                                      transforms.RandomRotation(
-                                          (-5.0, 5.0), fill=(1,)),
-                                      # transforms.RandomAffine((-5.0,5.0),fillcolor=1),
-                                      #transforms.RandomPerspective(),
-                                      transforms.ToTensor(),
-                                      # The mean and std have to be sequences (e.g., tuples), therefore you should add a comma after the values.
-                                      transforms.Normalize(
-                                          (0.1307,), (0.3081,))
-                                      ])
-test_transforms = transforms.Compose([
-    #  transforms.Resize((28, 28)),
-    #  transforms.ColorJitter(brightness=0.10, contrast=0.1, saturation=0.10, hue=0.1),
-    transforms.ToTensor(),
-    # The mean and std have to be sequences (e.g., tuples), therefore you should add a comma after the values.
-    transforms.Normalize((0.1307,), (0.3081,))
-])
-
-# dataloader arguments
-dataloader_args = dict(shuffle=True, batch_size=128, num_workers=4,
-                       pin_memory=True) if cuda else dict(shuffle=True, batch_size=64)
-
-# train dataloader
-train_loader = torch.utils.data.DataLoader(train, ** dataloader_args)
-
-# test dataloader
-test_loader = torch.utils.data.DataLoader(test, **dataloader_args)
+def get_transforms(type):
+    if type = 'train':
+        return transforms.Compose([
+                                        #  transforms.Resize((28, 28)),
+                                        #  transforms.ColorJitter(brightness=0.10, contrast=0.1, saturation=0.10, hue=0.1),
+                                        transforms.RandomRotation(
+                                            (-5.0, 5.0), fill=(1,)),
+                                        # transforms.RandomAffine((-5.0,5.0),fillcolor=1),
+                                        #transforms.RandomPerspective(),
+                                        transforms.ToTensor(),
+                                        # The mean and std have to be sequences (e.g., tuples), therefore you should add a comma after the values.
+                                        transforms.Normalize(
+                                            (0.1307,), (0.3081,))
+                                        ])
+    if type = 'test':
+        return transforms.Compose([
+        #  transforms.Resize((28, 28)),
+        #  transforms.ColorJitter(brightness=0.10, contrast=0.1, saturation=0.10, hue=0.1),
+        transforms.ToTensor(),
+        # The mean and std have to be sequences (e.g., tuples), therefore you should add a comma after the values.
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
 
 
-#Dataset stats & sample data
-train_data = train.train_data
-#train_data = train.transform(train.train_data.numpy())
+def get_dataloader(**dataloader_args):
+    # train dataloader
+    train_loader = torch.utils.data.DataLoader(train, ** dataloader_args)
 
-print('[Train]')
-print(' - Numpy Shape:', train.train_data.cpu().numpy().shape)
-print(' - Tensor Shape:', train.train_data.size())
-print(' - min:', torch.min(train_data))
-print(' - max:', torch.max(train_data))
-print(' - mean:', torch.Tensor.float(train_data).mean())
-print(' - std:', torch.Tensor.float(train_data).std())
-print(' - var:', torch.Tensor.float(train_data).var())
+    # test dataloader
+    test_loader = torch.utils.data.DataLoader(test, **dataloader_args)
 
-dataiter = iter(train_loader)
-images, labels = dataiter.next()
 
-#Data Properties
-# simple transform
-simple_transforms = transforms.Compose([
-    #  transforms.Resize((28, 28)),
-    #  transforms.ColorJitter(brightness=0.10, contrast=0.1, saturation=0.10, hue=0.1),
-                                       transforms.ToTensor(),
-                                       # The mean and std have to be sequences (e.g., tuples), therefore you should add a comma after the values.
-                                       transforms.Normalize(
-                                           (0.1307,), (0.3081,))
-                                       # Note the difference between (0.1307) and (0.1307,)
-                                       ])
-exp = datasets.MNIST('./data', train=True, download=True,
-                     transform=simple_transforms)
-exp_data = exp.train_data
-exp_data = exp.transform(exp_data.numpy())
+def get_data_stats(data):
+    #Dataset stats & sampledata
+    
+    print(' - Numpy Shape:', data.cpu().numpy().shape)
+    print(' - Tensor Shape:', data.size())
+    print(' - min:', torch.min(data))
+    print(' - max:', torch.max(data))
+    print(' - mean:', torch.Tensor.float(data).mean())
+    print(' - std:', torch.Tensor.float(data).std())
+    print(' - var:', torch.Tensor.float(data).var())
 
-print('[Train]')
-print(' - Numpy Shape:', exp.train_data.cpu().numpy().shape)
-print(' - Tensor Shape:', exp.train_data.size())
-print(' - min:', torch.min(exp_data))
-print(' - max:', torch.max(exp_data))
-print(' - mean:', torch.mean(exp_data))
-print(' - std:', torch.std(exp_data))
-print(' - var:', torch.var(exp_data))
+
+def get_data(data_loader)
+    dataiter = iter(train_loader)
+    images, labels = dataiter.next()
+    return images, lables
--- Session7/model.py
+++ Session7/model.py
@@ -116,3 +116,18 @@ def get_optimizer(loss_type):
     optimizer = optim.SGD(model.parameters(), lr= 0.01, momentum=0.9)
   return optimizer
 
+
+def run_model(model, device, optimiser, EPOCHS=1, is_L1_loss=False, is_GBN=False, gbn_splits=2):
+
+  train_losses = []
+  train_acc = []
+  test_losses = []
+  test_acc = []
+
+  for epoch in range(EPOCHS):
+      print("EPOCH:", epoch+1)
+      train(model, device, train_loader, optimizer, epoch,
+            train_losses, train_acc, is_L1_loss, lamda_l1=0.0001)
+      test(model, device, test_loader, test_losses, test_acc)
+
+  return {'train_loss': train_losses,  'train_acc': train_acc,  'test_loss': test_losses,  'test_acc': test_acc}
