ea491aa576cea77d6fd0861911bb669637d84882
Prachi Singh <prachi735@users.noreply.github.com>
2021-01-23 17:17:08 +0530

    Update dataset.py

--- Session7/dataset.py
+++ Session7/dataset.py
@@ -17,7 +17,7 @@ def get_transforms(mean, std):
     train = transforms.Compose([
                                         # transforms.Resize((28, 28)),
                                         # transforms.ColorJitter(brightness=0.10, contrast=0.1, saturation=0.10, hue=0.1),
-                                        transforms.RandomRotation((-5.0, 5.0)),
+                                        #transforms.RandomRotation((-5.0, 5.0)),
                                         # transforms.RandomAffine((-5.0,5.0),fillcolor=1),
                                         #transforms.RandomPerspective(),
                                         transforms.ToTensor(),
