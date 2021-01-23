b79ed5717a49f315d355f3b223fa64199d774373
Prachi Singh <prachi735@users.noreply.github.com>
2021-01-23 13:30:28 +0530

    Update model.py

--- Session7/model.py
+++ Session7/model.py
@@ -115,11 +115,11 @@ class Net(nn.Module):
 
 def get_optimizer(loss_type):
     if loss_type == "L2":
-    optimizer = optim.SGD(model.parameters(), lr=0.01,
-                          momentum=0, weight_decay=0, nesterov=False)
-  else:
-    optimizer = optim.SGD(model.parameters(), lr= 0.01, momentum=0.9)
-  return optimizer
+        optimizer = optim.SGD(model.parameters(), lr=0.01,
+                  momentum=0, weight_decay=0, nesterov=False)
+    else:
+        optimizer = optim.SGD(model.parameters(), lr= 0.01, momentum=0.9)
+    return optimizer
 
 
 def run_model(model, device, optimiser, EPOCHS=1, is_L1_loss=False, is_GBN=False, gbn_splits=2):
