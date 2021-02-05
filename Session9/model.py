9fc90ff33bace8b1b95214a1d6f5cd04a9223b2c
Prachi Singh <prachi735@users.noreply.github.com>
2021-02-05 18:17:00 +0530

    Update model.py

--- Session9/model.py
+++ Session9/model.py
@@ -8,65 +8,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class BasicBlock(nn.Module):
-    expansion = 1
-
-    def __init__(self, in_planes, planes, stride=1):
-        super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(
-            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
-                               stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion*planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes,
-                          kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*planes)
-            )
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.bn2(self.conv2(out))
-        out += self.shortcut(x)
-        out = F.relu(out)
-        return out
-
-
-class Bottleneck(nn.Module):
-    expansion = 4
-
-    def __init__(self, in_planes, planes, stride=1):
-        super(Bottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
-                               stride=stride, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, self.expansion *
-                               planes, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
-
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion*planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes,
-                          kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*planes)
-            )
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = F.relu(self.bn2(self.conv2(out)))
-        out = self.bn3(self.conv3(out))
-        out += self.shortcut(x)
-        out = F.relu(out)
-        return out
-
-
 class ResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes=10):
         super(ResNet, self).__init__()
@@ -102,26 +43,3 @@ class ResNet(nn.Module):
 
 def ResNet_18():
     return ResNet(BasicBlock, [2, 2, 2, 2])
-
-def run_model(model, device, optimiser,train_loader,test_loader, EPOCHS=1, is_L1_loss=False, is_GBN=False, gbn_splits=2):
-
-  train_losses = []
-  train_acc = []
-  test_losses = []
-  test_acc = []
-
-  for epoch in range(EPOCHS):
-      print("EPOCH:", epoch+1)
-      train(model, device, train_loader, optimizer, epoch,
-            train_losses, train_acc, is_L1_loss, lamda_l1=0.0001)
-      test(model, device, test_loader, test_losses, test_acc)
-
-  return {'train_loss': train_losses,  'train_acc': train_acc,  'test_loss': test_losses,  'test_acc': test_acc}
-
-def get_optimizer(model_parameters,loss_type,learn_rate):
-    if loss_type == "L2":
-        optimizer = optim.SGD(model_parameters, lr=0.01,
-                  momentum=0, weight_decay=0, nesterov=False)
-    else:
-        optimizer = optim.SGD(model_parameters, lr= 0.01, momentum=0.9)
-    return optimizer
