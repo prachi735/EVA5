d7492b6427a56653610fb21fb34554767b89d2a7
prachi735 <prachi735@gmail.com>
2021-02-05 11:34:34 +0000

    update img aug

--- Session9/dataset.py
+++ Session9/dataset.py
@@ -28,17 +28,17 @@ def get_transforms(mean, std):
         #     A.Blur(blur_limit=3, p=0.1),
         # ], p=0.2),
         A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.2, rotate_limit=45, p=0.2),
-        A.OneOf([
-            A.OpticalDistortion(p=0.3),
-            A.GridDistortion(p=.1),
-            A.IAAPiecewiseAffine(p=0.3),
-        ], p=0.2),
-        A.OneOf([
-            A.CLAHE(clip_limit=2),
-            A.IAASharpen(),
-            A.IAAEmboss(),
-            A.RandomBrightnessContrast(),            
-        ], p=0.3),
+        # A.OneOf([
+        #     A.OpticalDistortion(p=0.3),
+        #     A.GridDistortion(p=.1),
+        #     A.IAAPiecewiseAffine(p=0.3),
+        # ], p=0.2),
+        # A.OneOf([
+        #     A.CLAHE(clip_limit=2),
+        #     A.IAASharpen(),
+        #     A.IAAEmboss(),
+        #     A.RandomBrightnessContrast(),            
+        # ], p=0.3),
         A.HueSaturationValue(p=0.3),
         transforms.ToTensor(),
         transforms.Normalize(mean,std)
