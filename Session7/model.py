3e9347fd6b4482b2ed9e18ed53a0920899250f5b
Prachi735 <Prachi735@gmail.com>
2021-02-05 12:57:03 +0530

    Adding Session 9

--- Session7/model.py
+++ Session7/model.py
@@ -1,3 +1,6 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 class BatchNorm(nn.BatchNorm2d):
     def __init__(self, num_features, eps=1e-05, momentum=0.1, weight=True, bias=True):
         super().__init__(num_features, eps=eps, momentum=momentum)
