54dc696c0ec7936400695d6ce4c35c739af5f0fa
Prachi735 <Prachi735@gmail.com>
2021-02-06 14:09:25 +0530

    updating model

--- Session9/model.py
+++ Session9/model.py
@@ -1,6 +1,11 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
 class BatchNorm(nn.BatchNorm2d):
     def __init__(self, num_features, eps=1e-05, momentum=0.1, weight=True, bias=True):
         super().__init__(num_features, eps=eps, momentum=momentum)
@@ -8,6 +13,7 @@ class BatchNorm(nn.BatchNorm2d):
         self.bias.data.fill_(0.0)
         self.weight.requires_grad = weight
         self.bias.requires_grad = bias
+
 
 class GhostBatchNorm(BatchNorm):
     def __init__(self, num_features, num_splits, **kw):
@@ -44,21 +50,21 @@ class GhostBatchNorm(BatchNorm):
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, in_planes, planes,num_splits=1, stride=1):
+    def __init__(self, in_planes, planes, stride=1):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(
             in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = GhostBatchNorm(planes, num_splits=num_splits)
+        self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
                                stride=1, padding=1, bias=False)
-        self.bn2 = GhostBatchNorm(planes, num_splits)
+        self.bn2 = nn.BatchNorm2d(planes)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_planes, self.expansion*planes,
                           kernel_size=1, stride=stride, bias=False),
-                GhostBatchNorm(self.expansion*planes, num_splits)
+                nn.BatchNorm2d(self.expansion*planes)
             )
 
     def forward(self, x):
@@ -72,44 +78,48 @@ class BasicBlock(nn.Module):
 class Bottleneck(nn.Module):
     expansion = 4
 
-    def __init__(self, in_planes, planes, num_splits=1,stride=1):
+    def __init__(self, in_planes, planes, dropout,stride=1):
         super(Bottleneck, self).__init__()
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
-        self.bn1 = GhostBatchNorm(planes, num_splits=num_splits)
+        self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
                                stride=stride, padding=1, bias=False)
-        self.bn2 = GhostBatchNorm(planes, num_splits=num_splits)
+        self.bn2 = nn.BatchNorm2d(planes)
         self.conv3 = nn.Conv2d(planes, self.expansion *
                                planes, kernel_size=1, bias=False)
-        self.bn3 = GhostBatchNorm(self.expansion*planes, num_splits=num_splits)
+        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
+        self.dropout = nn.Dropout(dropout)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_planes, self.expansion*planes,
                           kernel_size=1, stride=stride, bias=False),
-                GhostBatchNorm(self.expansion*planes, num_splits)
+                nn.BatchNorm2d(self.expansion*planes)
             )
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.dropout(F.relu(self.bn1(self.conv1(x))))
+        out = self.dropout(F.relu(self.bn2(self.conv2(out))))
         out = self.bn3(self.conv3(out))
         out += self.shortcut(x)
         out = F.relu(out)
         return out
 
 class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_splits=1,num_classes=10):
+    def __init__(self, block, num_blocks, dropout, num_classes=10):
         super(ResNet, self).__init__()
         self.in_planes = 64
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
                                stride=1, padding=1, bias=False)
-        self.bn1 = GhostBatchNorm(64, num_splits=num_splits)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(
+            block, 64, num_blocks[0], dropout= 0.25, stride=1)
+        self.layer2 = self._make_layer(
+            block, 128, num_blocks[1], dropout=0.15, stride=2)
+        self.layer3 = self._make_layer(
+            block, 256, num_blocks[2], dropout=0.1, stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
         self.linear = nn.Linear(512*block.expansion, num_classes)
 
@@ -132,5 +142,5 @@ class ResNet(nn.Module):
         out = self.linear(out)
         return out
 
-def ResNet_18(num_splits):
-    return ResNet(BasicBlock, [2, 2, 2, 2], num_splits)
+def ResNet_18():
+    return ResNet(BasicBlock, [2, 2, 2, 2])
