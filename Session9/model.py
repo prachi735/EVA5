231c81b165921615feab3f8dd46e5eca424c4b00
Prachi735 <Prachi735@gmail.com>
2021-02-06 14:14:55 +0530

    updating model

--- Session9/model.py
+++ Session9/model.py
@@ -50,7 +50,7 @@ class GhostBatchNorm(BatchNorm):
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, in_planes, planes, stride=1):
+    def __init__(self, in_planes, planes, dropout=0.1,stride=1):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(
             in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
@@ -79,7 +79,7 @@ class BasicBlock(nn.Module):
 class Bottleneck(nn.Module):
     expansion = 4
 
-    def __init__(self, in_planes, planes, dropout,stride=1):
+    def __init__(self, in_planes, planes, dropout=0.1,stride=1):
         super(Bottleneck, self).__init__()
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
