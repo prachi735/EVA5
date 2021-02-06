ebe7c89bea59110f40ca304b622c5b1ef6fdd18d
Prachi735 <Prachi735@gmail.com>
2021-02-06 11:50:48 +0530

    adding optim module

---
+++ Session9/optim.py
@@ -0,0 +1,4 @@
+import torch.optim as optim
+optimizer = optim.SGD(resnet18_model.parameters(), lr=0.01,
+                      momentum=0.9, weight_decay=5e-4)
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
