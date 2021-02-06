661d24aab7556fd54db83767f0e6c23f9db52ac4
Prachi735 <Prachi735@gmail.com>
2021-02-06 17:41:35 +0530

    update transforms

--- Session9/transforms.py
+++ Session9/transforms.py
@@ -41,20 +41,20 @@ def get_album_transforms(norm_mean, norm_std):
             A.MedianBlur(blur_limit=3, p=0.1),
             A.Blur(blur_limit=3, p=0.1),
         ], p=0.2),
-        A.ShiftScaleRotate(shift_limit=0.0625,
-                           scale_limit=0.2, rotate_limit=45, p=0.2),
+        # A.ShiftScaleRotate(shift_limit=0.0625,
+        #                    scale_limit=0.2, rotate_limit=45, p=0.2),
         A.OneOf([
             A.OpticalDistortion(p=0.3),
             A.GridDistortion(p=.1),
             A.IAAPiecewiseAffine(p=0.3),
         ], p=0.2),
-        A.OneOf([
-            A.CLAHE(clip_limit=2),
-            A.IAASharpen(),
-            A.IAAEmboss(),
-            A.RandomBrightnessContrast(),
-        ], p=0.3),
-        A.HueSaturationValue(p=0.3),
+        # A.OneOf([
+        #     A.CLAHE(clip_limit=2),
+        #     A.IAASharpen(),
+        #     A.IAAEmboss(),
+        #     A.RandomBrightnessContrast(),
+        # ], p=0.3),
+        # A.HueSaturationValue(p=0.3),
         A.Normalize(mean=norm_mean, std=norm_std, always_apply=True, p=1.0),
         ToTensorV2()
     ])
