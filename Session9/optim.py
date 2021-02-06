49868359d732ec6b4ef79f591e50bb289a2d81fb
Prachi735 <Prachi735@gmail.com>
2021-02-06 11:56:25 +0530

    updating optim module

--- Session9/optim.py
+++ Session9/optim.py
@@ -1,4 +1,11 @@
 import torch.optim as optim
-optimizer = optim.SGD(resnet18_model.parameters(), lr=0.01,
-                      momentum=0.9, weight_decay=5e-4)
-scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
+
+
+def get_optimizer(model, lr=0.01,
+                  momentum=0.9, weight_decay=5e-4):
+
+    return optim.SGD(model.parameters(), lr=0.01,momentum=momentum, weight_decay=weight_decay)
+
+
+def get_scheduler(optimizer, T_max=200):
+    return torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=T_max)
