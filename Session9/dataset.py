0ef28783da517ebe4e22bef6e945d0e25ebbf3dc
Prachi735 <Prachi735@gmail.com>
2021-02-08 13:23:08 +0530

    update dataset

--- Session9/dataset.py
+++ Session9/dataset.py
@@ -10,27 +10,25 @@ class CIFARData(CIFAR10):
         super().__init__(path, train=train, download=download)
         self.transforms = transforms
 
-    def __getitem__(self, index):
+    def __getitem__(self, index: int) -> Tuple[Any, Any]:
         """
         Args:
             index (int): Index
 
         Returns:
-            tuple: (image, target) where target is index of the target class and Image is an tensor array
+            tuple: (image, target) where target is index of the target class.
         """
         img, target = self.data[index], self.targets[index]
-        
+
         # doing this so that it is consistent with all other datasets
         # to return a PIL Image
-        img = Image.fromarray(img)
+        #img = Image.fromarray(img)
 
         if self.transform is not None:
             img = self.transform(img)
 
         if self.target_transform is not None:
             target = self.target_transform(target)
-        
-        #img = torch.from_file(img)
 
         return img, target
 
