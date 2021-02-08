44690412d590e230424f16e9ce8f45255cf3c32f
Prachi735 <Prachi735@gmail.com>
2021-02-08 11:53:49 +0530

    update dataset

--- Session9/dataset.py
+++ Session9/dataset.py
@@ -11,10 +11,26 @@ class CIFARData(CIFAR10):
         self.transforms = transforms
 
     def __getitem__(self, index):
-        im, label = super().__getitem__(index)
-        if self.transforms:
-            im = self.transforms(image=im)
-        return im, label
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
+        img, target = self.data[index], self.targets[index]
+        
+        # doing this so that it is consistent with all other datasets
+        # to return a PIL Image
+        # img = Image.fromarray(img)
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
 
 def get_data(train_transforms, test_transforms):
     train = datasets.CIFAR10('./data', train=True, download=True,transform=train_transforms)
