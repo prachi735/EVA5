bcc58df4360a5419f1456cf520014594f15eee45
Prachi735 <Prachi735@gmail.com>
2021-02-09 15:36:44 +0530

    update dataset

--- Session9/dataset.py
+++ Session9/dataset.py
@@ -1,21 +1,15 @@
-from typing import Any, Tuple
 from torchvision.datasets import CIFAR10
 from torchvision import datasets
 import torch
 from PIL import Image
 import numpy as np
-
-
 class CIFARData(CIFAR10):
-    def __init__(self, path, train, download, transform=None) -> None:
-
-        super().__init__(path, train=train, download=download)
-        self.transform = transform
+    def __init__(self, path, train, download, transform=None):
     
-    def __len__(self) -> int:
-        return len(self.data)
+        super(CIFARData, self).__init__(path, train=train, download=download)
+        self.transform = transform
 
-    def __getitem__(self, index: int) -> Tuple[Any,Any]:
+    def __getitem__(self, index: int):
         """
         Args:
             index (int): Index
@@ -24,31 +18,33 @@ class CIFARData(CIFAR10):
             tuple: (image, target) where target is index of the target class.
         """
 
-        image, target = self.data[index], self.targets[index]
+        img, target = self.data[index], self.targets[index]
+        #img, target = super().__getitem__(index)
 
+        # doing this so that it is consistent with all other datasets
+        # to return a PIL Image
+        # img = Image.fromarray(img)
+        
         if self.transform is not None:
-            img = self.transform(image=image)['image']
+            img = self.transform(image=img)['image']
 
         if self.target_transform is not None:
             target = self.target_transform(target)
 
-        return image, target
-
+        return img, target
 
 def get_data(train_transforms, test_transforms):
-    train = datasets.CIFAR10('./data', train=True,
-                             download=True, transform=train_transforms)
-    test = datasets.CIFAR10('./data', train=False,
-                            download=True, transform=test_transforms)
+    train = datasets.CIFAR10('./data', train=True, download=True,transform=train_transforms)
+    test = datasets.CIFAR10('./data', train=False, download=True,transform=test_transforms)
     return train, test
 
 
 def get_dataloader(data, shuffle=True, batch_size=128, num_workers=4, pin_memory=True):
 
-    cuda = torch.cuda.is_available()
+  cuda = torch.cuda.is_available()
 
-    dataloader_args = dict(shuffle=shuffle, batch_size=batch_size, num_workers=num_workers,
-                           pin_memory=pin_memory) if cuda else dict(shuffle=True, batch_size=64)
-    dataloader = torch.utils.data.DataLoader(data, ** dataloader_args)
+  dataloader_args = dict(shuffle=shuffle, batch_size=batch_size, num_workers=num_workers,
+                         pin_memory=pin_memory) if cuda else dict(shuffle=True, batch_size=64)
+  dataloader = torch.utils.data.DataLoader(data, ** dataloader_args)
 
-    return dataloader
+  return dataloader
