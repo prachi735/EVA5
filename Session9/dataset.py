e091a4139924cd5c39965b58b08eeeafe5b3f45b
Prachi735 <Prachi735@gmail.com>
2021-02-09 18:41:18 +0530

    update dataset

--- Session9/dataset.py
+++ Session9/dataset.py
@@ -5,6 +5,26 @@ from PIL import Image
 import numpy as np
 
 
+# class AlbumentationsDataset(Dataset):
+#     """__init__ and __len__ functions are the same as in TorchvisionDataset"""
+
+#     def __init__(self, rimages, labels, transform=None):
+#         self.rimages = rimages
+#         self.labels = labels
+#         self.transform = transform
+
+#     def __len__(self):
+#         return len(self.rimages)
+
+#     def __getitem__(self, idx):
+#         label = self.labels[idx]
+#         image = self.rimages[idx]
+#         if self.transform:
+#             augmented = self.transform(image=image)
+#             image = augmented['image']
+#         return image, label
+
+
 class Cifar10AlbuDataset(datasets.CIFAR10):
     def __init__(self, root="~/data/cifar10", train=True, download=True, transform=None):
         super().__init__(root=root, train=train, download=download, transform=transform)
@@ -19,23 +39,23 @@ class Cifar10AlbuDataset(datasets.CIFAR10):
         return image, label
 
 
-def get_data(train_transforms, test_transforms, alb_dataset=True):
-    train_set = datasets.CIFAR10(
-        root='./data', train=True, download=True)
-    test_set = datasets.CIFAR10(
-        root='./data', train=False, download=True)
-    train_set = AlbumentationsDataset(
-        rimages=train_set.data,
-        labels=train_set.targets,
-        transform=train_transforms,
-    )
+# def get_data(train_transforms, test_transforms, alb_dataset=True):
+#     train_set = datasets.CIFAR10(
+#         root='./data', train=True, download=True)
+#     test_set = datasets.CIFAR10(
+#         root='./data', train=False, download=True)
+#     train_set = AlbumentationsDataset(
+#         rimages=train_set.data,
+#         labels=train_set.targets,
+#         transform=train_transforms,
+#     )
 
-    test_set = AlbumentationsDataset(
-        rimages=test_set.data,
-        labels=test_set.targets,
-        transform=test_transforms,
-    )
-    return train_set, test_set
+#     test_set = AlbumentationsDataset(
+#         rimages=test_set.data,
+#         labels=test_set.targets,
+#         transform=test_transforms,
+#     )
+#     return train_set, test_set
 
 
 def get_dataloader(data, shuffle=True, batch_size=128, num_workers=4, pin_memory=True):
