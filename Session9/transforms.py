6579b120625c69636d0c78e4c3b77a53f4ff41ad
Prachi735 <Prachi735@gmail.com>
2021-02-09 13:45:00 +0530

    update transforms

--- Session9/transforms.py
+++ Session9/transforms.py
@@ -1,6 +1,6 @@
 import albumentations as A
 from albumentations.pytorch.transforms import ToTensorV2 
-from torchvision import  transforms as tvt
+from torchvision import transforms
 
 
 def get_album_transforms(norm_mean, norm_std):
@@ -24,23 +24,19 @@ def get_album_transforms(norm_mean, norm_std):
 
 
 def get_torch_transforms(mean, std):
-    train = tvt.Compose([
-                        # transforms.Resize((28, 28)),
-                        tvt.ColorJitter(brightness=0.10, contrast=0.1, saturation=0.10, hue=0.1),
-                        tvt.RandomRotation((-5.0, 5.0)),
-                        # transforms.RandomAffine((-5.0,5.0),fillcolor=1),
-                        #transforms.RandomPerspective(),                    
-                        # The mean and std have to be sequences (e.g., tuples), therefore you should add a comma after the values.
-                        tvt.ToTensor(),
-                        tvt.Normalize((mean), (std,))
-                        
-                        ])
-    test = tvt.Compose([
-        #  transforms.Resize((28, 28)),
-        #  transforms.ColorJitter(brightness=0.10, contrast=0.1, saturation=0.10, hue=0.1),
-        # The mean and std have to be sequences (e.g., tuples), therefore you should add a comma after the values.
-        tvt.ToTensor(),
-        tvt.Normalize((mean,), (std,))
-        
-    ])
-    return train,test
+    train_transforms = transforms.Compose([
+                                      #  transforms.Resize((28, 28)),
+                                      #  transforms.ColorJitter(brightness=0.10, contrast=0.1, saturation=0.10, hue=0.1),
+                                       #transforms.RandomRotation((-5.0, 5.0), fill=(1,)),
+                                      # transforms.RandomAffine((-5.0,5.0),fillcolor=1),
+                                       #transforms.RandomPerspective(),
+                                       transforms.ToTensor(),
+                                       transforms.Normalize((0.1307,), (0.3081,)) # The mean and std have to be sequences (e.g., tuples), therefore you should add a comma after the values. 
+                                       ])
+    test_transforms = transforms.Compose([
+                                      #  transforms.Resize((28, 28)),
+                                      #  transforms.ColorJitter(brightness=0.10, contrast=0.1, saturation=0.10, hue=0.1),
+                                       transforms.ToTensor(),
+                                       transforms.Normalize((0.1307,), (0.3081,)) # The mean and std have to be sequences (e.g., tuples), therefore you should add a comma after the values. 
+                                       ])
+    return train_transforms,test_transforms
