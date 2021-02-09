6081a406403560b4f3c3f0c56923162c20728672
Prachi735 <Prachi735@gmail.com>
2021-02-09 18:01:01 +0530

    update dataset

--- Session9/dataset.py
+++ Session9/dataset.py
@@ -1,73 +1,41 @@
+from torch.utils.data.dataset import Dataset
+from torchvision import datasets
 import torch
-import torchvision
-
-import albumentations as A
-import albumentations.pytorch as AP
-from torch.utils.data import Dataset
-
+from PIL import Image
+import numpy as np
 
 class AlbumentationsDataset(Dataset):
-    """__init__ and __len__ functions are the same as in TorchvisionDataset"""
-
-    def __init__(self, rimages, labels, transform=None):
-        self.rimages = rimages
-        self.labels = labels
+    
+    def __init__(self, data, targets, classes, transform=None):
+        self.data = data
+        self.targets = targets
+        self.classes = classes
         self.transform = transform
 
     def __len__(self):
-        return len(self.rimages)
+        return len(self.data)
 
     def __getitem__(self, idx):
-        label = self.labels[idx]
-        image = self.rimages[idx]
+        image = self.data[idx]
+        target = self.targets[idx]
+
         if self.transform:
             augmented = self.transform(image=image)
             image = augmented['image']
-        return image, label
+        return image, target
 
 
-def get_album_transforms(norm_mean, norm_std):
-    """get the train and test transform by albumentations"""
-    album_train_transform = A.Compose([A.HorizontalFlip(p=.2),
-                                       A.VerticalFlip(p=.2),
-                                       A.Rotate(limit=15, p=0.5),
-                                       A.Normalize(
-        mean=[0.49, 0.48, 0.45],
-        std=[0.25, 0.24, 0.26], ),
-        AP.transforms.ToTensor()
-    ])
+def get_data(train_transforms, test_transforms, alb_dataset=True):
+    train = datasets.CIFAR10('./data', train=True,
+                             download=True, transform=train_transforms)
+    test = datasets.CIFAR10('./data', train=False,
+                            download=True, transform=test_transforms)
+    if alb_dataset:
+      print("creating AlbumentationsDataset.....")
+      train = AlbumentationsDataset(train.data, train.targets, train.classes, train_transforms)
+      test = AlbumentationsDataset(test.data, test.targets, test.classes, test_transforms)
+    return train, test
 
-    album_test_transform = A.Compose([A.Normalize(
-        mean=[0.49, 0.48, 0.45],
-        std=[0.25, 0.24, 0.26], ),
-        AP.transforms.ToTensor()
-    ])
-    return(album_train_transform, album_test_transform)
-
-
-def get_datasets():
-    """Extract and transform the data"""
-    train_set = torchvision.datasets.CIFAR10(
-        root='./data', train=True, download=True)
-    test_set = torchvision.datasets.CIFAR10(
-        root='./data', train=False, download=True)
-    return(train_set, test_set)
-
-
-def trasnform_datasets(train_set, test_set, train_transform, test_transform):
-    """Transform the data"""
-    train_set = AlbumentationsDataset(
-        rimages=train_set.data,
-        labels=train_set.targets,
-        transform=train_transform,
-    )
-
-    test_set = AlbumentationsDataset(
-        rimages=test_set.data,
-        labels=test_set.targets,
-        transform=test_transform,
-    )
-    return(train_set, test_set)
 
 def get_dataloader(data, shuffle=True, batch_size=128, num_workers=4, pin_memory=True):
 
