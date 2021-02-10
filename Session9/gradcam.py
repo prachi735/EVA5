d537e0c8b24f3a53a71825cdb4c6cd22e0a83873
Prachi735 <Prachi735@gmail.com>
2021-02-10 19:23:15 +0530

    update transform

--- Session9/gradcam.py
+++ Session9/gradcam.py
@@ -1,261 +1,112 @@
 import torch
-import torch.nn.functional as F
-import cv2
-import matplotlib.pyplot as plt
 import numpy as np
+import torch
+from torch.autograd import Function
 
-class GradCAM:
-    """Calculate GradCAM salinecy map.
-    Args:
-        input: Input image with shape of (1, 3, H, W)
-        class_idx: Class index for calculating GradCAM.
-            If not specified, the class index that makes the highest model prediction score will be used.
-    Returns:
-        mask: Saliency map of the same spatial dimension with input
-        logit: Model output
-    """
 
-    def __init__(self, model, layer_name):
+class FeatureExtractor():
+    """ Class for extracting activations and
+    registering gradients from targetted intermediate layers """
+
+    def __init__(self, model, target_layers):
         self.model = model
-        self.layer_name = layer_name
-        self._target_layer()
+        self.target_layers = target_layers
+        self.gradients = []
 
-        self.gradients = dict()
-        self.activations = dict()
+    def save_gradient(self, grad):
+        self.gradients.append(grad)
 
-        def backward_hook(module, grad_input, grad_output):
-            self.gradients['value'] = grad_output[0]
+    def __call__(self, x):
+        outputs = []
+        self.gradients = []
+        for name, module in self.model._modules.items():
+            x = module(x)
+            if name in self.target_layers:
+                x.register_hook(self.save_gradient)
+                outputs += [x]
+        return outputs, x
 
-        def forward_hook(module, input, output):
-            self.activations['value'] = output
 
-        self.target_layer.register_forward_hook(forward_hook)
-        self.target_layer.register_backward_hook(backward_hook)
+class ModelOutputs():
+    """ Class for making a forward pass, and getting:
+    1. The network output.
+    2. Activations from intermeddiate targetted layers.
+    3. Gradients from intermeddiate targetted layers. """
 
-    def _target_layer(self):
-        layer_num = int(self.layer_name.lstrip('layer'))
-        if layer_num == 1:
-            self.target_layer = self.model.layer1
-        elif layer_num == 2:
-            self.target_layer = self.model.layer2
-        elif layer_num == 3:
-            self.target_layer = self.model.layer3
-        elif layer_num == 4:
-            self.target_layer = self.model.layer4
+    def __init__(self, model, feature_module, target_layers):
+        self.model = model
+        self.feature_module = feature_module
+        self.feature_extractor = FeatureExtractor(
+            self.feature_module, target_layers)
 
-    def saliency_map_size(self, *input_size):
-        device = next(self.model.parameters()).device
-        self.model(torch.zeros(1, 3, *input_size, device=device))
-        return self.activations['value'].shape[2:]
+    def get_gradients(self):
+        return self.feature_extractor.gradients
 
-    def forward(self, input, class_idx=None, retain_graph=False):
-        b, c, h, w = input.size()
+    def __call__(self, x):
+        target_activations = []
+        for name, module in self.model._modules.items():
+            if module == self.feature_module:
+                target_activations, x = self.feature_extractor(x)
+            elif "avgpool" in name.lower():
+                x = module(x)
+                x = x.view(x.size(0), -1)
+            else:
+                x = module(x)
 
-        logit = self.model(input)
-        if class_idx is None:
-            score = logit[:, logit.max(1)[-1]].squeeze()
-        else:
-            score = logit[:, class_idx].squeeze()
+        return target_activations, x
 
+
+class GradCam:
+    def __init__(self, model, feature_module, target_layer_names, use_cuda):
+        self.model = model
+        self.feature_module = feature_module
+        self.model.eval()
+        self.cuda = use_cuda
+        if self.cuda:
+            self.model = model.cuda()
+
+        self.extractor = ModelOutputs(
+            self.model, self.feature_module, target_layer_names)
+
+    def forward(self, input_img):
+        return self.model(input_img)
+
+    def __call__(self, input_img, target_category=None):
+        if self.cuda:
+            input_img = input_img.cuda()
+
+        features, output = self.extractor(input_img)
+
+        if target_category == None:
+            target_category = np.argmax(output.cpu().data.numpy())
+
+        one_hot = np.zeros((1, output.size()[-1]), dtype=np.float32)
+        one_hot[0][target_category] = 1
+        one_hot = torch.from_numpy(one_hot).requires_grad_(True)
+        if self.cuda:
+            one_hot = one_hot.cuda()
+
+        one_hot = torch.sum(one_hot * output)
+
+        self.feature_module.zero_grad()
         self.model.zero_grad()
-        score.backward(retain_graph=retain_graph)
-        gradients = self.gradients['value']
-        activations = self.activations['value']
-        b, k, u, v = gradients.size()
+        one_hot.backward(retain_graph=True)
 
-        alpha = gradients.view(b, k, -1).mean(2)
-        weights = alpha.view(b, k, 1, 1)
+        grads_val = self.extractor.get_gradients()[-1].cpu().data.numpy()
 
-        saliency_map = (weights*activations).sum(1, keepdim=True)
-        saliency_map = F.relu(saliency_map)
-        saliency_map = F.upsample(saliency_map, size=(
-            h, w), mode='bilinear', align_corners=False)
-        saliency_map_min, saliency_map_max = saliency_map.min(), saliency_map.max()
-        saliency_map = (
-            saliency_map - saliency_map_min).div(saliency_map_max - saliency_map_min).data
+        target = features[-1]
+        target = target.cpu().data.numpy()[0, :]
 
-        return saliency_map, logit
+        weights = np.mean(grads_val, axis=(2, 3))[0, :]
+        cam = np.zeros(target.shape[1:], dtype=np.float32)
 
-    def __call__(self, input, class_idx=None, retain_graph=False):
-        return self.forward(input, class_idx, retain_graph)
+        for i, w in enumerate(weights):
+            cam += w * target[i, :, :]
+
+        cam = np.maximum(cam, 0)
+        cam = cv2.resize(cam, input_img.shape[2:])
+        cam = cam - np.min(cam)
+        cam = cam / np.max(cam)
+        return cam
 
 
-def to_numpy(tensor):
-    """Convert 3-D torch tensor to a 3-D numpy array.
-    Args:
-        tensor: Tensor to be converted.
-    """
-    return np.transpose(tensor.clone().numpy(), (1, 2, 0))
-
-
-def unnormalize(image, mean, std, out_type='array'):
-    """Un-normalize a given image.
-
-    Args:
-        image: A 3-D ndarray or 3-D tensor.
-            If tensor, it should be in CPU.
-        mean: Mean value. It can be a single value or
-            a tuple with 3 values (one for each channel).
-        std: Standard deviation value. It can be a single value or
-            a tuple with 3 values (one for each channel).
-        out_type: Out type of the normalized image.
-            If `array` then ndarray is returned else if
-            `tensor` then torch tensor is returned.
-    """
-
-    if type(image) == torch.Tensor:
-        image = np.transpose(image.clone().numpy(), (1, 2, 0))
-
-    normal_image = image * std + mean
-    if out_type == 'tensor':
-        return torch.Tensor(np.transpose(normal_image, (2, 0, 1)))
-    elif out_type == 'array':
-        return normal_image
-    return None  # No valid value given
-
-
-def visualize_cam(mask, img, alpha=1.0):
-    """Make heatmap from mask and synthesize GradCAM result image using heatmap and img.
-    Args:
-        mask (torch.tensor): mask shape of (1, 1, H, W) and each element has value in range [0, 1]
-        img (torch.tensor): img shape of (1, 3, H, W) and each pixel value is in range [0, 1]
-    Returns:
-        heatmap (torch.tensor): heatmap img shape of (3, H, W)
-        result (torch.tensor): synthesized GradCAM result of same shape with heatmap.
-    """
-
-    heatmap = (255 * mask.squeeze()).type(torch.uint8).cpu().numpy()
-    heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
-    heatmap = torch.from_numpy(heatmap).permute(2, 0, 1).float().div(255)
-    b, g, r = heatmap.split(1)
-    heatmap = torch.cat([r, g, b]) * alpha
-
-    result = heatmap + img.cpu()
-    result = result.div(result.max()).squeeze()
-
-    return heatmap, result
-
-
-class GradCAMView:
-
-    def __init__(self, model, layers, device, mean, std):
-        """Instantiate GradCAM and GradCAM++.
-        Args:
-            model: Trained model.
-            layers: List of layers to show GradCAM on.
-            device: GPU or CPU.
-            mean: Mean of the dataset.
-            std: Standard Deviation of the dataset.
-        """
-        self.model = model
-        self.layers = layers
-        self.device = device
-        self.mean = mean
-        self.std = std
-
-        self._gradcam()
-
-        print('Mode set to GradCAM.')
-        self.grad = self.gradcam.copy()
-
-        self.views = []
-
-    def _gradcam(self):
-        """ Initialize GradCAM instance. """
-        self.gradcam = {}
-        for layer in self.layers:
-            self.gradcam[layer] = GradCAM(self.model, layer)
-
-    def switch_mode(self):
-        if self.grad == self.gradcam:
-            print('Mode switched to GradCAM++.')
-            self.grad = self.gradcam_pp.copy()
-        else:
-            print('Mode switched to GradCAM.')
-            self.grad = self.gradcam.copy()
-
-    def _cam_image(self, norm_image):
-        """Get CAM for an image.
-        Args:
-            norm_image: Normalized image. Should be of type
-                torch.Tensor
-
-        Returns:
-            Dictionary containing unnormalized image, heatmap and CAM result.
-        """
-        norm_image_cuda = norm_image.clone().unsqueeze_(0).to(self.device)
-        heatmap, result = {}, {}
-        for layer, gc in self.gradcam.items():
-            mask, _ = gc(norm_image_cuda)
-            cam_heatmap, cam_result = visualize_cam(
-                mask,
-                unnormalize(norm_image, self.mean, self.std, out_type='tensor').clone(
-                ).unsqueeze_(0).to(self.device)
-            )
-            heatmap[layer], result[layer] = to_numpy(
-                cam_heatmap), to_numpy(cam_result)
-        return {
-            'image': unnormalize(norm_image, self.mean, self.std),
-            'heatmap': heatmap,
-            'result': result
-        }
-
-    def _plot_view(self, view, fig, row_num, ncols, metric):
-        """Plot a CAM view.
-        Args:
-            view: Dictionary containing image, heatmap and result.
-            fig: Matplotlib figure instance.
-            row_num: Row number of the subplot.
-            ncols: Total number of columns in the subplot.
-            metric: Can be one of ['heatmap', 'result'].
-        """
-        sub = fig.add_subplot(row_num, ncols, 1)
-        sub.axis('off')
-        plt.imshow(view['image'])
-        sub.set_title(f'{metric.title()}:')
-        for idx, layer in enumerate(self.layers):
-            sub = fig.add_subplot(row_num, ncols, idx + 2)
-            sub.axis('off')
-            plt.imshow(view[metric][layer])
-            sub.set_title(layer)
-
-    def cam(self, norm_image_list):
-        """Get CAM for a list of images.
-        Args:
-            norm_image_list: List of normalized images. Each image
-                should be of type torch.Tensor
-        """
-        for norm_image in norm_image_list:
-            self.views.append(self._cam_image(norm_image))
-
-    def plot(self, plot_path):
-        """Plot heatmap and CAM result.
-        Args:
-            plot_path: Path to save the plot.
-        """
-
-        for idx, view in enumerate(self.views):
-            # Initialize plot
-            fig = plt.figure(figsize=(10, 10))
-
-            # Plot view
-            self._plot_view(view, fig, 1, len(self.layers) + 1, 'result')
-
-            # Set spacing and display
-            fig.tight_layout()
-            plt.show()
-
-            # Save image
-            fig.savefig(f'{plot_path}_{idx + 1}.png', bbox_inches='tight')
-
-            # Clear cache
-            plt.clf()
-
-    def __call__(self, norm_image_list, plot_path):
-        """Get GradCAM for a list of images.
-        Args:
-            norm_image_list: List of normalized images. Each image
-                should be of type torch.Tensor
-        """
-        self.cam(norm_image_list)
-        self.plot(plot_path)
--- Session9/transforms.py
+++ Session9/transforms.py
@@ -11,28 +11,10 @@ def get_album_transforms(norm_mean, norm_std):
         A.RandomRotate90(),
         A.Flip(),
         A.Transpose(),
-        A.OneOf([
-            A.IAAAdditiveGaussianNoise(),
-            A.GaussNoise(),
-        ], p=0.2),
-        A.OneOf([
-            A.MotionBlur(p=.2),
-            A.MedianBlur(blur_limit=3, p=0.1),
-            A.Blur(blur_limit=3, p=0.1),
-        ], p=0.2),
         A.ShiftScaleRotate(shift_limit=0.0625,
                            scale_limit=0.2, rotate_limit=45, p=0.2),
-        A.OneOf([
-            A.OpticalDistortion(p=0.3),
-            A.GridDistortion(p=.1),
-            A.IAAPiecewiseAffine(p=0.3),
-        ], p=0.2),
-        A.OneOf([
-            A.CLAHE(clip_limit=2),
-            A.IAASharpen(),
-            A.IAAEmboss(),
-            A.RandomBrightnessContrast(),
-        ], p=0.3),
+        A.CLAHE(clip_limit=2),
+        A.RandomBrightnessContrast(),
         A.HueSaturationValue(p=0.3),
         A.Normalize(mean=norm_mean, std=norm_std ),
         ToTensorV2()
