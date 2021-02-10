a7c9c6e87a82fb9ef806adb9d54f0327b98c1835
Prachi735 <Prachi735@gmail.com>
2021-02-10 14:25:21 +0530

    update test train

--- Session9/test.py
+++ Session9/test.py
@@ -10,12 +10,12 @@ def test(model, device, test_loader, loss_fn) -> Tuple[float, float]:
             data, target = data.to(device), target.to(device)
             output = model(data)
             # sum up batch loss
-            test_loss = loss_fn(output, target)
+            test_loss = loss_fn(output, target).item()
             # get the index of the max log-probability
             pred = output.argmax(dim=1, keepdim=True)
             correct += pred.eq(target.view_as(pred)).sum().item()
 
-    test_loss /= len(test_loader.dataset)
+    #test_loss /= len(test_loader.dataset)
 
     accuracy = 100. * correct / len(test_loader.dataset)
 
