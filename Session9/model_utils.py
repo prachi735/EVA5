598406e7e6205477f167a6319834435da156a241
Prachi735 <Prachi735@gmail.com>
2021-02-11 18:00:49 +0530

    update transforms

--- Session9/model_utils.py
+++ Session9/model_utils.py
@@ -10,8 +10,11 @@ def get_optimizer(model, lr=0.01,
     return optim.SGD(model.parameters(), lr=0.01,momentum=momentum, weight_decay=weight_decay)
 
 
-def get_scheduler(optimizer, T_max=200):
+def get_scheduler(ReduceLROnPlateau, timizer, T_max=200):
+    if type == 
+    return torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min')
     return torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=T_max)
+
 
 
 def get_loss_function():
