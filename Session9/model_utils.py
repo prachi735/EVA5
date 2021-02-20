aa00b7331f3f91613caa3bfe0ea42cc32f0b8121
Prachi735 <Prachi735@gmail.com>
2021-02-20 12:24:10 +0530

    update model utils

--- Session9/model_utils.py
+++ Session9/model_utils.py
@@ -2,6 +2,7 @@ import torch.nn as nn
 import torch
 import torch.optim as optim
 from torchsummary import summary
+from torch.optim import lr_scheduler
 from model import ResNet_18
 
 def get_optimizer(model, lr=0.01,
@@ -10,10 +11,23 @@ def get_optimizer(model, lr=0.01,
     return optim.SGD(model.parameters(), lr=0.01,momentum=momentum, weight_decay=weight_decay)
 
 
-def get_scheduler(optimizer):    
-    return torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min')
-    #return torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=T_max)
-
+def get_scheduler(args, optimizer, datasets):
+    if args.scheduler == 'step':
+        scheduler = lr_scheduler.MultiStepLR(
+            optimizer, milestones=eval(args.milestones), gamma=args.lr_decay)
+    elif args.scheduler == 'poly':
+        total_step = (len(datasets['train']) / args.batch + 1) * args.epochs
+        scheduler = lr_scheduler.LambdaLR(
+            optimizer, lambda x: (1-x/total_step) ** args.power)
+    elif args.scheduler == 'plateau':
+        scheduler = lr_scheduler.ReduceLROnPlateau(
+            optimizer, mode='max', factor=args.lr_decay, patience=args.patience)
+    elif args.scheduler == 'constant':
+        scheduler = lr_scheduler.LambdaLR(optimizer, lambda x: 1)
+    elif args.scheduler == 'cosine':
+        scheduler = lr_scheduler.CosineAnnealingLR(
+            optimizer, args.T_max, args.min_lr)
+    return scheduler
 
 
 def get_loss_function():
