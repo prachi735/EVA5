62e86c56eb173251b5e26bf19c90745cd0ca708f
Prachi735 <Prachi735@gmail.com>
2021-02-20 12:06:24 +0530

    update test

--- Session9/test.py
+++ Session9/test.py
@@ -1,5 +1,6 @@
 from typing import Tuple
 import torch
+import torch.functional as F
 
 def test(model, device, test_loader, loss_fn) -> Tuple[float, float]:
     model.eval()
@@ -10,11 +11,13 @@ def test(model, device, test_loader, loss_fn) -> Tuple[float, float]:
             data, target = data.to(device), target.to(device)
             output = model(data)
             # sum up batch loss
-            test_loss = loss_fn(output, target).item()
+            test_loss += loss_fn(output, target).item()
+            # test_loss += F.nll_loss(output, target, reduction='sum').item()
             # get the index of the max log-probability
             pred = output.argmax(dim=1, keepdim=True)
             correct += pred.eq(target.view_as(pred)).sum().item()
-
+    
+    test_loss /= len(test_loader.dataset)
     accuracy = 100. * correct / len(test_loader.dataset)
 
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
